1cf878246cd7c418ed13d0ab734ed0dee9c52742
Jovith Ngoho | Crazy Domains <Jovith.Ngoho@crazydomains.com>
2018-10-22 18:59:17 +0800

    update 102218

--- resources/views/layouts/layouts_admin/admin_header.blade.php
+++ resources/views/layouts/layouts_admin/admin_header.blade.php
@@ -8,7 +8,7 @@
 <!--top-Header-menu-->
 <div id="user-nav" class="navbar navbar-inverse">
   <ul class="nav">
-    <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome User</span><b class="caret"></b></a>
+    <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">{{auth()->user()->name}}</span><b class="caret"></b></a>
       <ul class="dropdown-menu">
         <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
         <li class="divider"></li>
@@ -17,7 +17,6 @@
         <li><a href="{{ route('logout') }}"><i class="icon-key"></i> Log Out</a></li>
       </ul>
     </li>
-    <li class=""><a title="" href="#"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
     <li class=""><a title="" href="{{ route('logout') }}"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
   </ul>
 </div>
